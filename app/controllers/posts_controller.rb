9a783190802439414c162b74e69b21c3108649d0
Trenton Greer <yoshigreer@gmail.com>
2017-02-18 10:23:55 -0700

    assigment 18 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts[0].title = "spam"
+    @posts.each do |post|
+      post.title = "spam" if (post.id % 5) == 0
+    end
   end
 
   def show
@@ -11,4 +15,5 @@ class PostsController < ApplicationController
 
   def edit
   end
+
 end
