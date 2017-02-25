5df2050149713e6d5d9bae1a5274fe25179f40e9
Trenton Greer <yoshigreer@gmail.com>
2017-02-24 20:53:21 -0700

    assignment 23 complete

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: name, description: description) }
 
   it { is_expected.to have_many(:posts) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_length_of(:name).is_at_least(5) }
+  it { is_expected.to validate_length_of(:description).is_at_least(20) }
 
   describe "attributes" do
     it "has name, description, and public attributes" do
