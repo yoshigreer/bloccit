6ceeddcaaa7ff702103fef67474d9f7269d24da6
Trenton Greer <yoshigreer@gmail.com>
2017-02-25 18:56:31 -0700

    working on assignment

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -54,4 +54,37 @@ RSpec.describe UsersController, type: :controller do
       expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
     end
   end
+
+  describe "POST confirm" do
+    it "returns an http redirect" do
+      post :confirm, user: new_user_attributes
+      expect(response).to have_http_status(:success)
+    end
+
+    it "creates a new user" do
+      expect {
+        post :confirm, user: new_user_attributes
+      }.to change(User, :count).by(1)
+    end
+
+    it "sets user name properly" do
+      post :confirm, user: new_user_attributes
+      expect(assigns(:user).name).to eq new_user_attributes[:name]
+    end
+
+    it "sets user email properly" do
+      post :confirm, user: new_user_attributes
+      expect(assigns(:user).email).to eq new_user_attributes[:email]
+    end
+
+    it "sets user password properly" do
+      post :confirm, user: new_user_attributes
+      expect(assigns(:user).password).to eq new_user_attributes[:password]
+    end
+
+    it "sets user password_confirmation properly" do
+      post :confirm, user: new_user_attributes
+      expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
+    end
+  end
 end
