a1fdb5f5116f6453a86d157872be1ff19ef523ad
Trenton Greer <yoshigreer@gmail.com>
2017-03-09 18:35:38 -0700

    checkpoint 28 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
 
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.role ||= :member }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -10,4 +11,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  enum role: [:member, :admin]
 end
