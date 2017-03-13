febd999e8a33117c5dc1461269a17ea1a7d9190a
Trenton Greer <yoshigreer@gmail.com>
2017-03-12 19:53:39 -0600

    checkpoint 30 complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312190502) do
+ActiveRecord::Schema.define(version: 20170313013428) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170312190502) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -79,5 +80,16 @@ ActiveRecord::Schema.define(version: 20170312190502) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
